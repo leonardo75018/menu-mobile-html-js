93f6199754599d55299507c7423602f4d1d1ba6e
leonardo kabongo <leonardinho@MacBook-Pro-de-leonardo.local>
2022-09-06 10:46:38 +0200

    -

---
+++ app.js
@@ -0,0 +1,11 @@
+const btnMobile = document.querySelector("#btn-mobile");
+
+const toggleMenu = () =>{
+    const nav = document.querySelector("#nav");
+    nav.classList.toggle("active");
+
+}
+
+
+
+btnMobile.addEventListener("click", toggleMenu);
